f3c78e30d8fdb446b41eb3ed7737c312ce7db524
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-11 14:48:02 -0300

    feat: types for string basic

---
+++ types/string.ts
@@ -0,0 +1,5 @@
+// implied
+let implied = "quando a constante e definida sua tipagem e de acordo com o valor que ela recebe" 
+
+//explicit
+let explicit: string = "aqui você esta deixando de fora explicita qual e sua tipagem" 
