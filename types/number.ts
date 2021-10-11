9f5b324c35f731acb625bf51059add5bc1867f4a
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-11 15:26:31 -0300

    feat: type number

---
+++ types/number.ts
@@ -0,0 +1,7 @@
+// implied
+let impliedNumber = 0
+console.log("🚀 ~ file: string.ts ~ line 3 ~ implied", impliedNumber)
+
+//explicit
+let explicitNumber: number = 10
+console.log("🚀 ~ file: string.ts ~ line 5 ~ explicit", explicitNumber)
--- types/string.ts
+++ types/string.ts
@@ -1,5 +1,7 @@
 // implied
-let implied = "quando a constante e definida sua tipagem e de acordo com o valor que ela recebe" 
+let impliedString = ":quando a constante e definida sua tipagem e de acordo com o valor que ela recebe" 
+console.log("🚀 ~ file: string.ts ~ line 3 ~ implied", impliedString)
 
 //explicit
-let explicit: string = "aqui você esta deixando de fora explicita qual e sua tipagem" 
+let explicitString: string = ":aqui você esta deixando de fora explicita qual e sua tipagem" 
+console.log("🚀 ~ file: string.ts ~ line 5 ~ explicit", explicitString)
