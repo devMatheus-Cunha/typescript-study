a0f707dea3bddc21e9e051561baafb64b1216636
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-13 12:28:26 -0300

    feat: types tuplas  explicit

---
+++ types/tuplas.ts
@@ -0,0 +1,11 @@
+//* success
+let andress: [string, number, boolean] = ["Av portugal", 168, false];
+andress = ["rua moura costa", 166, true];
+
+//! error
+// let andress: [boolean, number, string] = ["Av portugal", 168, true]
+// andress = [166, "rua moura costa", true];
+
+console.log("====================================");
+console.log(andress);
+console.log("====================================");
