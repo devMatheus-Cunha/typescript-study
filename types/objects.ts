56f50095af7ac6a26e5ec8e8d4e650bc5610f737
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-13 16:55:00 -0300

    feat: types object

---
+++ types/objects.ts
@@ -0,0 +1,23 @@
+let user = {
+  name: "",
+  age: 0,
+};
+
+user = {
+  name: "Pedro",
+  age: 29
+}
+
+let andressUser: {name: string, age: number};
+andressUser = {
+  name: "Matheus",
+  age: 21
+}
+
+console.log("====================================");
+console.log(user);
+console.log("====================================");
+
+console.log("====================================");
+console.log(andressUser);
+console.log("====================================");
