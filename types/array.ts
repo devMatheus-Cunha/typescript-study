a796293e142bae0acfbaa98e92769f7422e39277
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-13 12:20:05 -0300

    feat: types explicit array

---
+++ types/array.ts
@@ -0,0 +1,21 @@
+let hobbiesOne: Array<string> = []
+let hobbiesTwo: string[] = []
+
+let agesOne: Array<number> = []
+let agesTwo: number[] = []
+
+//! error
+// hobbiesOne = [100]
+// agesTwo = ['100']
+
+//* success
+hobbiesOne = ["dev", "ui/ux", "engenheiro"]
+agesTwo = [19, 40, 23, 50]
+
+console.log('====================================');
+console.log(hobbiesOne[1]);
+console.log('====================================');
+
+console.log('====================================');
+console.log(agesTwo[3]);
+console.log('====================================');
