72be00ef657a298db3b621f2a43d95570c2c68a3
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-13 12:41:43 -0300

    feat: ex enums

---
+++ types/enums.ts
@@ -0,0 +1,19 @@
+enum Color {
+  cinza,
+  verde, 
+  azul
+}
+
+let myColor: Color = Color.cinza
+
+console.log('====================================');
+console.log(Color.azul);
+console.log('====================================');
+
+console.log('====================================');
+console.log(Color[2]);
+console.log('====================================');
+
+console.log('====================================');
+console.log(myColor);
+console.log('====================================');
