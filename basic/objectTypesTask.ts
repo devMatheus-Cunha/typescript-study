7555642d331c027b72ca3485bec92af97404b90d
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-14 15:29:07 -0300

    feat: object Types Task

---
+++ basic/objectTypesTask.ts
@@ -0,0 +1,23 @@
+let supervisors: {
+  employees : string[]
+  clockIn: (time: number) => string
+};
+
+supervisors = {
+  employees: ["Matheus", "Lucas", "Pedro"],
+  clockIn: (time: number): string  => {
+    if (time <= 8) {
+      return "Ponto normal";
+    } else {
+      return "Fora de horário";
+    }
+  }
+}
+
+console.log('====================================');
+console.log(supervisors.employees);
+console.log('====================================');
+
+console.log('====================================');
+console.log(supervisors.clockIn(10));
+console.log('====================================');
