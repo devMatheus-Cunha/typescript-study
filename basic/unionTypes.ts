7939f105f8f16a8b37c1c551956f301aec87a53d
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-14 15:59:57 -0300

    feat: add union types

---
+++ basic/unionTypes.ts
@@ -0,0 +1,6 @@
+let note: string | number = 10 
+note = "10"
+
+console.log('====================================');
+console.log(`Minha note é ${note}`);
+console.log('====================================');
