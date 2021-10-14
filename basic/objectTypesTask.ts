3e55510b5958d169680889c45142915383ee30b0
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-14 15:40:01 -0300

    feat: add ISupervisors dinamic

--- basic/objectTypesTask.ts
+++ basic/objectTypesTask.ts
@@ -1,23 +1,41 @@
+type ISupervisors = {
+  employees: string[];
+  clockIn: (time: number) => string;
+};
+
 let supervisors: {
-  employees : string[]
-  clockIn: (time: number) => string
+  employees: string[];
+  clockIn: (time: number) => string;
 };
 
 supervisors = {
   employees: ["Matheus", "Lucas", "Pedro"],
-  clockIn: (time: number): string  => {
+  clockIn: (time: number): string => {
     if (time <= 8) {
       return "Ponto normal";
     } else {
       return "Fora de horário";
     }
-  }
-}
+  },
+};
 
-console.log('====================================');
+let supervisors2: ISupervisors;
+
+supervisors = {
+  employees: ["Matheus", "Lucas", "Pedro"],
+  clockIn: (time: number): string => {
+    if (time <= 8) {
+      return "Ponto normal";
+    } else {
+      return "Fora de horário";
+    }
+  },
+};
+
+console.log("====================================");
 console.log(supervisors.employees);
-console.log('====================================');
+console.log("====================================");
 
-console.log('====================================');
+console.log("====================================");
 console.log(supervisors.clockIn(10));
-console.log('====================================');
+console.log("====================================");
