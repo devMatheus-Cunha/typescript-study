b75339cd9f0d99307119a76d76a8281e584a7d4b
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-15 10:26:01 -0300

    feat: useing noEmitOnError

---
+++ compiler/compiler.ts
@@ -0,0 +1,9 @@
+//!"noEmitOnError": true =  Disable emitting files if any type checking errors are reported.
+let chanel: string = "Gaveta"
+let subscribers: number = 31231
+
+// !error = chanel = subscribers
+console.log("🚀 ~ file: compiler.ts ~ line 4 ~ chanel", chanel)
+
+// !error = name space in more blocks
+// let name: string = "Matheus"
