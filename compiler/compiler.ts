580564169b577c98d34baeb64c8168e5c4aac0b4
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-15 11:09:41 -0300

    feat :explanation of noImplicitAny

--- compiler/compiler.ts
+++ compiler/compiler.ts
@@ -1,9 +1,9 @@
 //? "noEmitOnError": true =  Disable emitting files if any type checking errors are reported.
-let chanel: string = "Gaveta"
-let subscribers: number = 31231
+let chanel: string = "Gaveta";
+let subscribers: number = 31231;
 
 // !error = chanel = subscribers
-console.log("🚀 ~ file: compiler.ts ~ line 4 ~ chanel", chanel)
+console.log("🚀 ~ file: compiler.ts ~ line 4 ~ chanel", chanel);
 
 // !error = name space in more blocks
 // let name: string = "Matheus"
@@ -14,4 +14,21 @@ console.log("🚀 ~ file: compiler.ts ~ line 4 ~ chanel", chanel)
 
 //===========================================
 
-//? "sourceMap": true = Create source map files for emitted JavaScript files. 
+//? "sourceMap": true = Create source map files for emitted JavaScript files.
+
+//===========================================
+
+//? "noImplicitAny": true =  Enable error reporting for expressions and declarations with an implied `any` type
+
+function sum(a: any, b: any) {
+  console.log(a + b);
+}
+sum(2, 4);
+
+let ramdom
+ramdom = "Pedro"
+ramdom = 12
+
+//===========================================
+
+//? "noImplicitAny": true =  Enable error reporting for expressions and declarations with an implied `any` type
