3afa637e5111ff47652c590e76c0e570a8eac003
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-25 08:52:53 -0300

    feat: destructuring Array

---
+++ NewECMAScript/destructuringArray.ts
@@ -0,0 +1,12 @@
+// comum
+const caracteres = ["motor fiat", 2021];
+const motorOne = caracteres[0];
+const anoOne = caracteres[1];
+
+console.log(`O ${motorOne} e do ano ${anoOne} `);
+console.log('====================================');
+
+// destructuring
+const [motorTwo, anoTwo] = caracteres
+console.log(`O ${motorTwo} e do ano ${anoTwo} `);
+
