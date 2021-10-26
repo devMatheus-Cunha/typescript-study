b3ca3d7121e1648b042d49b3637e95fa88e57ea3
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-26 14:34:09 -0300

    feat: Task

--- classObjects/classAndAttribute .ts
+++ classObjects/classAndAttribute .ts
@@ -31,5 +31,23 @@ class DataSmart {
 const birthdaySmart = new DataSmart(19, 8, 2000);
 const MonthAndYearSmart = new DataSmart(8, 12);
 
-console.log(birthdaySmart);
-console.log(MonthAndYearSmart);
+// console.log(birthdaySmart);
+// console.log(MonthAndYearSmart);
+
+//? Task 
+//* Attributes: name, price, desc
+//* Create a constructur
+//* OBS 1: opctional discount ( value default 0)
+//* Create two products: passing two or three params
+
+class Product{
+  constructor(public name: string, public price: number, public desc: number = 0){}
+}
+
+const pruductOne = new Product("Xioami", 1900, 0.5)
+const pruductTwo = new Product("Iphone", 12222200)
+
+console.log('====================================');
+console.log(pruductOne);
+console.log('====================================');
+console.log(pruductTwo);
