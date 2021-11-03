f6b777477181861f7d73ca1677f988c81b4ce635
Matheus Gomes <gomesprogramacao@gmail.com>
2021-11-03 15:08:20 -0300

    feat: access modifiers

---
+++ classObjects/accessModifiers.ts
@@ -0,0 +1,35 @@
+class Car {
+  private currentSpeed: number = 0;
+
+  constructor(
+    public brand: string,
+    public model: string,
+    private maxSpeed: number = 200
+  ) {}
+
+  private changeSpeed(delta: number): number {
+    const newSpeed = this.currentSpeed + delta;
+    const validateSpeed = newSpeed >= 0 && newSpeed < this.maxSpeed;
+
+    if (validateSpeed) {
+      this.currentSpeed = newSpeed;
+    } else {
+      this.currentSpeed = delta > 0 ? this.maxSpeed : 0;
+    }
+    return this.currentSpeed;
+  }
+
+  public speedUp(): number {
+    return this.changeSpeed(5);
+  }
+
+  public breke() {
+    return this.changeSpeed(-5);
+  }
+}
+
+const carOne = new Car("Fiat", "Argo", 180)
+Array(40).fill(0).forEach(() => carOne.speedUp())
+console.log(carOne.speedUp())
+
+console.log(carOne.breke())
