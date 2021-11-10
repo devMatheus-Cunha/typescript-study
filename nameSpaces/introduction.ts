9b58641f87e43c4648026a3e0f3b78efdac96dd5
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-11-10 16:17:55 -0300

    feat: namespaces nested

--- nameSpaces/introduction.ts
+++ nameSpaces/introduction.ts
@@ -1,16 +1,17 @@
-namespace Area {
-  const PI = 3.14;
+namespace Geometry {
+  export namespace Area {
+    const PI = 3.14;
 
-  export function circumference(ray: number): number {
-    return PI * Math.pow(ray, 2);
-  }
-  
-  export function rectangle(base: number, height: number): number {
-    return base * height;
+    export function circumference(ray: number): number {
+      return PI * Math.pow(ray, 2);
+    }
+
+    export function rectangle(base: number, height: number): number {
+      return base * height;
+    }
   }
 }
 
-
-console.log(Area.circumference(10));
-console.log('====================================');
-console.log(Area.rectangle(10, 20));
+console.log(Geometry.Area.circumference(10));
+console.log("====================================");
+console.log(Geometry.Area.rectangle(10, 20));
