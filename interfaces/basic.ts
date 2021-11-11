7fc0c98134effdb7ad9254fab5f09547662b7d28
devMatheus-Cunha <devmatheusgr@gmail.com>
2021-11-11 15:43:11 -0300

    feat:  accept any property name and value

--- interfaces/basic.ts
+++ interfaces/basic.ts
@@ -1,20 +1,26 @@
 interface IPersonProps {
-  name: string,
-  age: number
+  name: string;
+  age?: number;
+  [prop: string] : any // accept any property name and value
 }
 
-const person: IPersonProps  = {
+const person: IPersonProps = {
   name: "Matheus",
-  age: 21
-}
+  age: 21,
+};
 
 function greetWithHello(person: IPersonProps) {
-  console.log(`Hello ${person.name}`)
+  console.log(`Hello ${person.name}`);
 }
 
 function chageName(person: IPersonProps) {
-  return person.name = "Pedro"
+  return (person.name = "Pedro");
 }
 
-greetWithHello(person)
-console.log(chageName(person))
+greetWithHello(person);
+console.log(chageName(person));
+greetWithHello({
+  name: "Lucas",
+  age: 32,
+  randomProps: true
+});
